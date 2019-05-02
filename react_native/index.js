c897778426aa5d4734e7e19da75d489d995a2fc1
ZamirAmiri <zamir.amiri@hotmail.com>
2019-05-02 14:32:54 +0200

    Gebruik goede commit namen niet iets zoals 2nd commit fzo zeg wat je  er in gedaan hebt

--- react_native/index.js
+++ react_native/index.js
@@ -5,8 +5,6 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
-import IntroCarousel from './IntroCarousel';
-
 import AppContainer from './StackNavigation';
 
 AppRegistry.registerComponent(appName, () => AppContainer);
